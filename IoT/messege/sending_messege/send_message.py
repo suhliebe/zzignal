f1de11afeb474bd45e80828629bab7731aca274e
yang <yanghuigeon9@gmail.com>
2020-12-12 16:57:07 +0900

    2020-12-12 fix sending_messege

--- IoT/messege/sending_messege/send_message.py
+++ IoT/messege/sending_messege/send_message.py
@@ -9,18 +9,16 @@ sys.path.append(os.path.split(libdir)[0])
 
 from auth import auth
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-apiKey = config['AUTH']['ApiKey']
-apiSecret = config['AUTH']['ApiSecret']
+apiKey = 'NCSLIPZ9VIPR1W3N'
+apiSecret = 'O6RMZCI4WYHO5DZKYBCBTEBRQHVMRYKL'
 
 if __name__ == '__main__':
     data = {
         'message': {
-            'to': '01092119647',#119라던가
+            'to': '01092119647',#119라던가 보낼번호
             'from': '01092119647',#"인증받은 번호만 됩니다"
             'text': 'test'#여기에 주소랑 입력하면 될거같습니다
         }
     }
-    res = requests.post(config['SERVER']['URI'] + 'send', headers=auth.get_headers(apiKey, apiSecret), json=data)
+    res = requests.post('https://api.solapi.com/messages/v4/' + 'send', headers=auth.get_headers(apiKey, apiSecret), json=data)
     print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
